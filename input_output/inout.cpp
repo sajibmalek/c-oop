0d1e8af8c37038db7c483f75486fcbe52ef9dc7a
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2022-05-16 23:04:55 +0600

    input and output
    std:: cout and std::cin done

---
+++ input_output/inout.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    std:: cout<< "Enter two number" <<std::endl;
+    int n1,n2;
+    std::cin>> n1>>n2;
+    std:: cout<<"The sum of" <<" "<<n1 <<" " <<"and" <<" "<<n2 <<" =" << n1+n2 <<std:: endl;
+    return 0;
+}
