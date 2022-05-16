0fbd4618627db5cd1d633ad420db620b96326334
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2022-05-16 23:20:27 +0600

    Update mean.cpp

--- mean/mean.cpp
+++ mean/mean.cpp
@@ -3,8 +3,8 @@
 int main(){
     std::cout<< "Enter three number" <<std::endl;
     double n1,n2,n3,mean; //variable declaration 
-    std::cin >> n1>>n2>>n3;
+    std::cin >> n1>>n2>>n3; //taking input from users
     mean=(n1+n2+n3)/3;// simplify for calculation 
-    std:: cout<< "Mean of three numbers " <<mean <<std::endl;
+    std:: cout<< "Mean of three numbers " <<mean <<std::endl; // output 
     return 0;
 }
