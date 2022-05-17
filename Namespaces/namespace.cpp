393a615472f393e81aef1e9977e1d0251d6eb2c4
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2022-05-17 22:23:08 +0600

    namespaces

---
+++ Namespaces/namespace.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// int main(){
+
+//      cout<<"iostream";
+//     return 0;
+// }
+// in this code show a error for std cout. beacuse we are not use std libary for cout... 
+//we can slove this problem by using namespace
+#include<iostream>
+using namespace std;
+int main(){
+
+      cout<<"namespace"  ;
+    return 0;
+}
