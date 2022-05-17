1e7e680fa56e0f60ede12ab5af7a409a9fb925b8
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2022-05-17 23:19:21 +0600

    while loop
    this program print will print 10 times huha with number

--- loops/while.cpp
+++ loops/while.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main (){
 int count =1;
-while(count <=10){
-    cout<< "huha" <<" " << count << endl;
+while(count <=10 ){
+    cout<< "huha" <<" " << count<< endl;
     count +=1;
- }
+  
+    
+
     return 0;
 }
    
